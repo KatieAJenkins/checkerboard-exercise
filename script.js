c52d3db3ad06d4848fdb1a933b3eab328ee57bce
KatieAJenkins <kind2karma@gmail.com>
2017-05-15 20:05:09 -0600

    cleaned up code

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+'use strict';
 // Your JS goes here
+var body = document.getElementsByTagName('body');
+// console.log(body);
+
+var div = document.createElement('div');
+  div.style.height = "100px";
+  div.style.width = "100px";
+  div.style.background = 'red';
+  div.style.bottomPadding
+console.log(div);
+
+// console.log(div);
+// var div2 = div.cloneNode(true);
+
+document.body.appendChild(div);
+
+
+// console.log(body);
